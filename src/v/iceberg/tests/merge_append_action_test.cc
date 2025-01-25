a11ae4022de3b62551d13d6d9faf45f9036c0018
Evgeny Lazin <4lazin@gmail.com>
2025-01-25 05:47:52 -0500

    cloud_storage: Move s3_imposter to cloud_io
    The s3_imposter is used in several subsystems already
    (iceberg/datalake/cloud_storage/cloud_topics). It makes sense to move it
    into cloud_io to make dependencies less confusing.
    
    Signed-off-by: Evgeny Lazin <4lazin@gmail.com>

--- src/v/iceberg/tests/merge_append_action_test.cc
+++ src/v/iceberg/tests/merge_append_action_test.cc
@@ -7,8 +7,8 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0
 #include "cloud_io/remote.h"
+#include "cloud_io/tests/s3_imposter.h"
 #include "cloud_io/tests/scoped_remote.h"
-#include "cloud_storage/tests/s3_imposter.h"
 #include "iceberg/manifest_entry.h"
 #include "iceberg/manifest_io.h"
 #include "iceberg/merge_append_action.h"
